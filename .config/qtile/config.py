e37b14cd2893315f37c8ae14d7a428233197d96a
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-06-16 17:22:31 -0500

    moved bar to top

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -107,15 +107,15 @@ layouts = [
 ]
 
 widget_defaults = dict(
-    font='sans',
-    fontsize=12,
-    padding=3,
+    font='Cascadia Code',
+    fontsize=14,
+    padding=1,
 )
 extension_defaults = widget_defaults.copy()
 
 screens = [
     Screen(
-        bottom=bar.Bar(
+        top=bar.Bar(
             [
                 widget.CurrentLayout(),
                 widget.GroupBox(),
@@ -127,10 +127,9 @@ screens = [
                     },
                     name_transform=lambda name: name.upper(),
                 ),
-                widget.TextBox("default config", name="default"),
                 widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
                 widget.Systray(),
-                widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
+                widget.Clock(format='%d/%m/%Y - %H:%M'),
                 widget.QuickExit(),
             ],
             24,
