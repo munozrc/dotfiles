6f02c346daae6cf5a1230c01cb45a54d2c663cef
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-06-23 21:18:10 -0500

    new groups qtile bar

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -9,23 +9,7 @@ from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 
 from keys import mod, keys
-
-groups = [Group(i) for i in "123456789"]
-
-for i in groups:
-    keys.extend([
-        # mod1 + letter of group = switch to group
-        Key([mod], i.name, lazy.group[i.name].toscreen(),
-            desc="Switch to group {}".format(i.name)),
-
-        # mod1 + shift + letter of group = switch to & move focused window to group
-        Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
-            desc="Switch to & move focused window to group {}".format(i.name)),
-        # Or, use below if you prefer not to switch to that group.
-        # # mod1 + shift + letter of group = move focused window to group
-        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-        #     desc="move focused window to group {}".format(i.name)),
-    ])
+from groups import groups
 
 layouts = [
     layout.MonadTall(border_focus="#e06c75", margin=12),
