438b0406b8c3523d987df8101c9027eb5e92a6ad
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-06-26 23:42:38 -0500

    create layouts file qtile config

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -9,12 +9,8 @@ from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 
 from keys import mod, keys
+from layouts import layouts, floating_layout
 from groups import groups
-
-layouts = [
-    layout.MonadTall(border_focus="#61afef", margin=20),
-    layout.Max(),
-]
 
 widget_defaults = dict(
     font="Hack Nerd Font",
@@ -66,16 +62,6 @@ dgroups_app_rules = []  # type: List
 follow_mouse_focus = True
 bring_front_click = False
 cursor_warp = False
-floating_layout = layout.Floating(float_rules=[
-    # Run the utility of `xprop` to see the wm class and name of an X client.
-    *layout.Floating.default_float_rules,
-    Match(wm_class='confirmreset'),  # gitk
-    Match(wm_class='makebranch'),  # gitk
-    Match(wm_class='maketag'),  # gitk
-    Match(wm_class='ssh-askpass'),  # ssh-askpass
-    Match(title='branchdialog'),  # gitk
-    Match(title='pinentry'),  # GPG key password entry
-])
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
