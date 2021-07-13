40372442b1e1a0018c1088fbb9abd18f1ee82c5c
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-07-12 22:19:40 -0500

    remove import never used qtile config

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -3,7 +3,6 @@ from typing import List  # noqa: F401
 from libqtile import bar, layout, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
-from libqtile.utils import guess_terminal
 
 mod = "mod4"
 terminal = "xterm" #guess_terminal()
